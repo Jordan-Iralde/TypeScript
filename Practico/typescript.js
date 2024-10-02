15207a4466b2d7503798dc3a0c5cd6414ba05113
Jordan-Iralde <iraldejordan10@gmail.com>
2024-10-02 09:55:57 -0300

    Ts

--- Practico/typescript.js
+++ Practico/typescript.js
@@ -1,9 +1,5 @@
-
 //Primero que nada necesito saber como obtener un valor de un input en js
 //Eso se hace con document.getElementByID(Id).value como STR;
-
-botonDePuntos = document.getElementsByClassName("BotonPuntos").value;
-
 
 function AgregarPuntos() {
     var Handball = document.getElementById("Handball_A").value;
@@ -16,20 +12,23 @@ function AgregarPuntos() {
     var AjedrezB = document.getElementById("Ajedrez_B").value;
 
     //Existe un Error si esta vacio
+
     
     var TotalPuntajesA = parseInt(Handball) + parseInt(Resistencia) + parseInt(Ajedrez);
     var TotalPuntajesB = parseInt(HandballB) + parseInt(ResistenciaB) + parseInt(AjedrezB);
     //Con parseInt o parseFloat convierto STR a numeros y los sumo
     
-    return TotalPuntajesA, TotalPuntajesB;
+    if (TotalPuntajesA > TotalPuntajesB){
+        console.log("Gano la Negra")
+    }
+    else if (TotalPuntajesB > TotalPuntajesA){
+        console.log("Gano la Roja")
+    }
+    else    {
+        console.log("Empate")
+    }
 }
-
 
 console.log("hola");
-console.log(botonDePuntos)
 //tsc --init Para iniciar un tsconfig.json
 //tsc typescript.ts para Compilarlo y ver JS
-if (botonDePuntos){
-    console.log("El total equipo A", AgregarPuntos());
-    console.log("El total equipo B", AgregarPuntos());
-}
--- Practico/typescript.ts
+++ Practico/typescript.ts
@@ -1,9 +1,24 @@
-var Handball_A = document.getElementById("Handball_A");
+//inteface
 
-function AgregarPuntos() {
+//puntosa
+
+//puntosb
+
+const Handball_A = document.getElementById("Handball_A") as HTMLInputElement;
+
+function AgregarPuntos(): void { //por ser funcion
     var TotalPuntajes = Handball_A
     console.log(TotalPuntajes);
     }
 
+function algo() : void {
+    var HandballA : number = parseInt(Handball_A.value, 10) //el 10 es radix pero no se si hace falta
+}
+
+    HTMLElement
+    HTMLInputElement
+
+
+    //innerText
 //tsc --init Para iniciar un tsconfig.json
 //tsc typescript.ts para Compilarlo y ver JS
